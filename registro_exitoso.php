ee72f6f5b5d0d350754b80ea006e4801d5724b7f
Ramagammer <84947133+Ramagammer@users.noreply.github.com>
2023-11-15 17:39:28 -0300

    Add files via upload

---
+++ registro_exitoso.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Registro exitoso</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&family=EB+Garamond&display=swap" rel="stylesheet">
+</head>
+<body>
+     <div class="principal-1">
+     	<div>
+     	<h1>¡Te has registrado correctamente!</h1>
+     	</div>
+     	<div>
+     	<a href="https://www.google.com" class="redirigir">
+            Ir a Google
+        </a>
+        </div>
+        <div class="box-3"></div>
+            <div class="logo-text-2">Developed by:</div>
+            <div id="logo-text-2" class="logo-text-3">Ramiro Hernandez</div>
+        </div> 
+     </div>
+     <script src="funciones.js"></script>
+</body>
+</html>
---
+++ registro_fallido.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro Fallido</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+     
+</body>
+</html>
