3f07564ebe0f285be101349e56f01366f7883fd5
Ramagammer <84947133+Ramagammer@users.noreply.github.com>
2023-11-15 17:35:05 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
         logoText1.style.visibility = "visible";
     }, 2500);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    setTimeout(function() {
+        var logoText2 = document.getElementById("logo-text-2");
+        logoText2.style.visibility = "visible";
+    }, 2500);
+});
